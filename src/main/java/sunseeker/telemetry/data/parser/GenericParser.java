07c9eb1b19be656248d2ddaafa96c5fa1b179b8c
alecgunnar <alecgunnar@gmail.com>
2017-06-24 15:02:41 -0400

    Added generic parser

--- src/main/java/sunseeker/telemetry/data/parser/GenericParser.java
+++ src/main/java/sunseeker/telemetry/data/parser/GenericParser.java
@@ -1,14 +1,53 @@
 package sunseeker.telemetry.data.parser;
 
-import java.util.Map;
+import java.nio.ByteBuffer;
+import java.util.*;
 
 /**
  * Created by aleccarpenter on 6/11/17.
  */
 public class GenericParser implements Parser {
+    private List<String> ignore;
+
+    public GenericParser() {
+        ignore = Arrays.asList("ABCDEF", "UVWXYZ");
+    }
+
     @Override
     public Map<String, Double> pushData(String data) {
+        Map<String, Double> parsed = new HashMap<>();
 
-        return null;
+        String[] lines = data.split("\n");
+
+        for (String line : lines) processLine(line, parsed);
+
+        return parsed;
+    }
+
+    private void processLine(String line, Map<String, Double> parsed) {
+        String[] parts = line.split(",");
+
+        if (ignore.indexOf(parts[0]) > -1) return;
+
+        parsed.put(parts[0] + "_0", parseFloatFromHex(parts[1]));
+        parsed.put(parts[0] + "_1", parseFloatFromHex(parts[2]));
+    }
+
+    private double parseFloatFromHex(String hex) {
+        String digits = hex.substring(2);
+        long value = Long.parseLong(digits, 16);
+
+        byte[] bytes = {
+                processByteAtOffset(value, 0),
+                processByteAtOffset(value, 8),
+                processByteAtOffset(value, 16),
+                processByteAtOffset(value, 24)
+        };
+
+        return ByteBuffer.wrap(bytes).getFloat();
+    }
+
+    protected byte processByteAtOffset (long val, int offset) {
+        return (byte) ((val >> offset) & 0xFF);
     }
 }
--- src/test/java/sunseeker/telemetry/data/parser/GenericParserTest.java
+++ src/test/java/sunseeker/telemetry/data/parser/GenericParserTest.java
@@ -1,10 +1,77 @@
 package sunseeker.telemetry.data.parser;
 
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
  * Created by aleccarpenter on 6/24/17.
  */
 public class GenericParserTest {
+    private GenericParser subject;
 
+    private String singleLineOfSampleData = "ABCDEF\n" +
+            "ABC,0x00000000,0xA7C3F942\n" +
+            "UVWXYZ";
+
+    private String firstHalfOfSampleData = "ABCDEF\n" +
+            "DEF,0x00000000,0xA7C3F942\n";
+
+    private String secondHalfOfSampleData = "GHI,0x00000000,0xA7C3F942\n" +
+            "UVWXYZ";
+
+    private Map<String, Double> result;
+
+    @Before
+    public void setup() {
+        subject = new GenericParser();
+
+        result = subject.pushData(singleLineOfSampleData);
+    }
+
+    @Test
+    public void should_returnAMapWithKeyZero_whenDataIsPushed() {
+        assertThat(result.containsKey("ABC_0"), is(true));
+    }
+
+    @Test
+    public void should_returnAMapWithKeyOne_whenDataIsPushed() {
+        assertThat(result.containsKey("ABC_1"), is(true));
+    }
+
+    @Test
+    public void should_mapTheFirstValueToKeyZero_whenDataIsPushed() {
+        assertThat(result.get("ABC_0"), is(0.0));
+    }
+
+    @Test
+    public void should_mapTheSecondValueToKeyOne_whenDataIsPushed() {
+        // TODO: make this assertion less fragile
+        assertThat(result.get("ABC_1"), is(124.88213348388672));
+    }
+
+    @Test
+    public void should_returnsFirstHalfOfData_givenSplitPage_whenDataIsPushed() {
+        Map<String, Double> result;
+
+        result = subject.pushData(firstHalfOfSampleData);
+
+        assertTrue(
+                result.containsKey("DEF_0")
+                        && result.containsKey("DEF_1")
+        );
+    }
+
+    @Test
+    public void should_returnsSecondHalfOfData_givenSplitPage_whenDataIsPushed() {
+        result = subject.pushData(secondHalfOfSampleData);
+
+        assertTrue(
+                result.containsKey("GHI_0")
+                && result.containsKey("GHI_1")
+        );
+    }
 }
